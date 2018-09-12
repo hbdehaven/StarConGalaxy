37b833ac3e0f40df735069fdabda3a9fdb9453ba
Heather DeHaven <hbdehaven@gmail.com>
2018-09-12 08:23:12 -0700

    P1

---
+++ src/ui/main.java
@@ -0,0 +1,23 @@
+package ui;
+
+public class main {
+
+    public static void main(String[] args) {
+        starsYeah();
+        sayAnd();
+        conStell();
+    }
+
+    public static void starsYeah() {
+        System.out.println("STARS!");
+    }
+
+    public static void conStell() {
+        System.out.println("CONSTELLATIONS!");
+    }
+
+    public static void sayAnd(){
+        System.out.println(" and ");
+    }
+
+}
