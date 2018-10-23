1e0f16dcd7e72b5ec26099bcd509d0cf6fe18272
Heather DeHaven <hbdehaven@gmail.com>
2018-10-23 15:20:38 -0700

    fixed my ListOfStarCOn tests from forever ago

--- test/ListOfStarConstellationTest.java
+++ test/ListOfStarConstellationTest.java
@@ -14,65 +14,60 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ListOfStarConstellationTest {
     // inspired by TPD-lecture-starters
 
-    ListOfStarConstellation listofstarcons;
+    private ListOfStarConstellation test;
+    private StarConstellation starcon1;
+    private StarConstellation starcon2;
 
     @BeforeEach
     public void setUp(){
-//        StarConstellation starcon1 = new StarConstellation("",0,true,"",null, null);
-//        StarConstellation starcon2 = new StarConstellation("",0,false,"",null, null);
-        listofstarcons = new ListOfStarConstellation("Star Cons");
-//        listofstarcons.add(starcon1);
-//        listofstarcons.add(starcon2);
+        test = new ListOfStarConstellation("test");
+        starcon1 = new StarConstellation("",0, StarConstellation.Location.NORTH,"",null);
+        starcon2 = new StarConstellation("",0, StarConstellation.Location.SOUTH,"",null);
     }
 
     @Test
     public void testsortNorthWithNorth(){
-        StarConstellation starcon1 = new StarConstellation("",0, StarConstellation.Location.NORTH,"",null);
-        listofstarcons.add(starcon1);
-        assertTrue(listofstarcons.contains(starcon1));
+        test.add(starcon1);
+        test.getNorth();
+        assertTrue(test.contains(starcon1));
     }
 
     @Test
     public void testsortNorthWithoutNorth(){
-        StarConstellation starcon2 = new StarConstellation("",0, StarConstellation.Location.SOUTH,"",null);
-        assertFalse(listofstarcons.contains(starcon2));
+        test.add(starcon2);
+        test.getNorth();
+        assertFalse(test.contains(starcon2));
     }
 
     @Test
     public void testsortNorthWithNorthandWithout(){
-        StarConstellation starcon1 = new StarConstellation("",0, StarConstellation.Location.NORTH,"",null);
-        StarConstellation starcon2 = new StarConstellation("",0, StarConstellation.Location.SOUTH,"",null);
-        listofstarcons.add(starcon1);
-        assertTrue(listofstarcons.contains(starcon1));
-        assertFalse(listofstarcons.contains(starcon2));
+        test.add(starcon1);
+        test.getNorth();
+        assertTrue(test.contains(starcon1));
+        assertFalse(test.contains(starcon2));
     }
 
     @Test
     public void testsortSouthWithOutSouth(){
-        StarConstellation starcon1 = new StarConstellation("",0, StarConstellation.Location.NORTH,"",null);
-        if (starcon1.getLocation() == StarConstellation.Location.SOUTH){
-            listofstarcons.add(starcon1);
-        }
-        assertFalse(listofstarcons.contains(starcon1));
+        test.add(starcon1);
+        test.getSouth();
+        assertFalse(test.contains(starcon1));
     }
 
     @Test
     public void testsortSouthWithSouth(){
-        StarConstellation starcon2 = new StarConstellation("",0, StarConstellation.Location.SOUTH,"",null);
-        if (starcon2.getLocation() == StarConstellation.Location.SOUTH){
-            listofstarcons.add(starcon2);
-        }
-        assertTrue(listofstarcons.contains(starcon2));
+        test.add(starcon2);
+        test.getSouth();
+        assertTrue(test.contains(starcon2));
     }
 
     @Test
     public void testsortSouthWithSouthandWithout(){
-        StarConstellation starcon1 = new StarConstellation("",0, StarConstellation.Location.NORTH,"",null);
-        StarConstellation starcon2 = new StarConstellation("",0, StarConstellation.Location.SOUTH,"",null);
-        if (starcon1.getLocation() == StarConstellation.Location.NORTH) {}
-        else if (starcon2.getLocation() == StarConstellation.Location.SOUTH) {listofstarcons.add(starcon2);}
-        assertFalse(listofstarcons.contains(starcon1));
-        assertTrue(listofstarcons.contains(starcon2));
+        test.add(starcon1);
+        test.add(starcon2);
+        test.getSouth();
+        assertFalse(test.contains(starcon1));
+        assertTrue(test.contains(starcon2));
     }
 
 }
